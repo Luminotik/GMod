0652753d6bcf63f7bacf2431bfcb74fc2369dfcc
Luminotik <nick.nestor@me.com>
2020-08-22 00:26:44 -0700

    removed unknown commands

--- etc/entry.sh
+++ etc/entry.sh
@@ -17,14 +17,11 @@ bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
 	+tv_port "${SRCDS_TV_PORT}" \
 	+clientport "${SRCDS_CLIENT_PORT}" \
 	+maxplayers "${SRCDS_MAXPLAYERS}" \
-	+game_mode "${SRCDS_GAMEMODE}" \
 	+map "${SRCDS_STARTMAP}" \
 	+sv_setsteamaccount "${SRCDS_TOKEN}" \
 	+rcon_password "${SRCDS_RCONPW}" \
 	+sv_password "${SRCDS_PW}" \
 	+sv_region "${SRCDS_REGION}" \
-	+net_public_adr "${SRCDS_NET_PUBLIC_ADDRESS}" \
 	-ip "${SRCDS_IP}" \
 	+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
-	+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
 	-authkey "${SRCDS_WORKSHOP_AUTHKEY}"
